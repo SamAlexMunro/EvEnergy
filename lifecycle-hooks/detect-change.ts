b51d6f252a0490b0f993752cc508fde8ce353ae8
sammunro94 <sam.munro@tepeo.com>
2024-08-07 22:28:59 +0100

    misc(): Remove unused starter code

--- lifecycle-hooks/detect-change.ts
+++
@@ -1,5 +0,0 @@
-import { useEffect } from 'react';
-
-export const detectChange = (...args: any[]) => {
-  useEffect(() => {}, args);
-};
--- lifecycle-hooks/on-init.ts
+++
@@ -1,5 +0,0 @@
-import { useEffect } from 'react';
-
-export const onInit = (callback: Function) => {
-  useEffect(() => callback(), []);
-};
