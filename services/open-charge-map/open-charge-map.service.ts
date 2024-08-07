3098e0322db0ad1df40bb9a6b86f6dc40776b772
sammunro94 <sam.munro@tepeo.com>
2024-08-07 14:32:53 +0100

    feat(OpenChargeMapService): Create simple retrievePoiList method

--- services/open-charge-map/open-charge-map.service.ts
+++ services/open-charge-map/open-charge-map.service.ts
@@ -1,1 +1,16 @@
-export class OpenChargeMapService {}
+import { HttpService } from '../http-service/http.service';
+
+const OPEN_CHARGE_MAP_API_KEY = 'ac2e74af-b6c6-421b-aa8a-c7b4d3f0635c';
+
+export class OpenChargeMapService {
+  constructor(private readonly HttpService: HttpService) {
+    this.retrievePoiList();
+  }
+
+  async retrievePoiList() {
+    const response = await this.HttpService.get({
+      url: `https://api.openchargemap.io/v3/poi?key=${OPEN_CHARGE_MAP_API_KEY}`,
+    });
+    console.log(response);
+  }
+}
