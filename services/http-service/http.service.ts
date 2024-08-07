ef76413b8d90c501aa34141fa24e9fb5a04bcaae
sammunro94 <sam.munro@tepeo.com>
2024-08-07 22:18:49 +0100

    feat(HttpService): Extend HTTPService to include put and post requests

--- services/http-service/http.service.ts
+++ services/http-service/http.service.ts
@@ -1,17 +1,21 @@
 import { BehaviorSubject } from 'rxjs';
 
+export enum Method {
+  POST = 'POST',
+  PUT = 'PUT',
+  GET = 'GET',
+}
 /**
  * HTTPService Wrapper allows us to encapsulate standardised logic
  * around HTTP requests, this can be further augmented or used as a template
- * if working with a bespoke internal BE service that has specific needs and responses
+ * if working with a bespoke internal BE service that has specific needs and responses.
  */
 export class HttpService {
-  error$ = new BehaviorSubject('');
-  loading$ = new BehaviorSubject(false);
+  readonly error$ = new BehaviorSubject('');
+  readonly loading$ = new BehaviorSubject(false);
 
   async get<Type>({ url, configurationOptions }: { url: string; configurationOptions?: {} }): Promise<Type> {
-    this.loading$.next(true);
-    this.error$.next('');
+    this.setSubjectDefaultStates();
 
     return fetch(url, configurationOptions)
       .then((response) => response.json())
@@ -19,12 +23,42 @@ export class HttpService {
       .finally(() => this.loading$.next(false));
   }
 
-  /**
-   * Example methods if we are building out a fully fledged HTTPService
-   */
-  post() {}
+  async post<Type>({
+    url,
+    body,
+    configurationOptions,
+  }: {
+    url: string;
+    body: {};
+    configurationOptions?: {};
+  }): Promise<Type> {
+    this.setSubjectDefaultStates();
 
-  put() {}
+    return fetch(url, { method: Method.POST, body: JSON.stringify(body), ...configurationOptions })
+      .then((response) => response.json())
+      .catch((error) => this.error$.next(error))
+      .finally(() => this.loading$.next(false));
+  }
 
-  update() {}
+  async put<Type>({
+    url,
+    body,
+    configurationOptions,
+  }: {
+    url: string;
+    body: {};
+    configurationOptions?: {};
+  }): Promise<Type> {
+    this.setSubjectDefaultStates();
+
+    return fetch(url, { method: Method.PUT, body: JSON.stringify(body), ...configurationOptions })
+      .then((response) => response.json())
+      .catch((error) => this.error$.next(error))
+      .finally(() => this.loading$.next(false));
+  }
+
+  private setSubjectDefaultStates() {
+    this.loading$.next(true);
+    this.error$.next('');
+  }
 }
